f90e958074eb88085b8a577b60247fc2c7eecbb7
bpellens <bpellens@gmail.com>
2015-04-13 11:35:47 +0200

    Add some basic tests

--- ATLSharp/ATLSharp.Tests/UnitTest1.cs
+++ ATLSharp/ATLSharp.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Antlr4.Runtime.Tree;
+using System.IO;
 
 namespace ATLSharp.Tests
 {
@@ -7,8 +9,31 @@ namespace ATLSharp.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ParseCorrectTransformation()
         {
+            IATLTransformationParser parser = new ATLTransformationParser();
+            IParseTree result = parser.Parse("../../Families2Persons.atl");
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ParseATLFromString()
+        {
+            var content = File.ReadAllText("../../Families2Persons.atl");
+
+            IATLTransformationParser parser = new ATLTransformationParser();
+            IParseTree result = parser.ParseString(content);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException), "Could not load the ATL file.")]
+        public void ParseIncorrectTransformation()
+        {
+            IATLTransformationParser parser = new ATLTransformationParser();
+            IParseTree result = parser.Parse("DummyFamilies2Persons.atl");
         }
     }
 }
